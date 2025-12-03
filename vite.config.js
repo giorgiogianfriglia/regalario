b6b6e865d2c7f5aedded4765e06916483a6f4eb8
Giorgio Gianfriglia <giorgio.gianfriglia@itvalues.it>
2025-12-03 09:30:30 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // <--- Importante
 
 export default defineConfig({
-  base: '/regalario/',
   plugins: [
     react(),
     tailwindcss(), // <--- Importante
